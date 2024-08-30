504fbef9434e021e5731146565184be93993a5e0
Johannes Edmeier <johannes.edmeier@steadybit.com>
2024-08-30 08:48:32 +0200

    chore: update oapi-codegen

--- go/event_kit_api/build.sh
+++ go/event_kit_api/build.sh
@@ -7,7 +7,8 @@
 
 set -eo pipefail
 
-go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0
-oapi-codegen -config generator-config.yml ../../openapi/spec.yml > event_kit_api.go
+go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.3.0
+oapi-codegen -config generator-config.yml -o event_kit_api.go ../../openapi/spec.yml
 
 cat extras.go.txt >> event_kit_api.go
+go fmt event_kit_api.go
